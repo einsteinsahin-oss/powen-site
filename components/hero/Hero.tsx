71d3a79a02dbb3f28848f63942d4d365159c93b6
einsteinsahin-oss <einstein.sahin@gmail.com>
2026-03-11 23:32:37 +0300

    Remove image filter, darken overlay

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -54,7 +54,7 @@ export default function Hero() {
               alt={slide.title}
               className="w-full h-full object-cover"
             />
-            <div className="absolute inset-0 bg-gradient-to-r from-black/60 via-black/80 to-transparent" />
+            <div className="absolute inset-0 bg-gradient-to-r from-black/50 via-black/30 to-transparent" />
           </div>
         ))}
       </div>
